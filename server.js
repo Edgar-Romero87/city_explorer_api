aa2b4fb039910ccb0bf059477ce42c7c1694a510
Edgar Romero <edgaromero1987@gmail.com>
2020-06-09 22:46:03 -0700

    added API key for trials

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 /* eslint-disable no-undef */
 'use strict';
 // express library sets up our server
@@ -33,56 +35,82 @@ app.get('/location', (request, response) => {
     })
 
 })
-// catch(err) {
-//     console.log('ERROR', err);
-//     response.status(500).send('sorry, we messed up');
-// }
 
-
-
-
-
-//WEATHER
-// app.get('/weather', (request, response) => {
-//   try {
-
-//     let geoData = require('./data/weather.json')
-//     let weatherArray = geoData.data.map(day => {
-//       return new Weather(day);
-//     })
-//     response.status(200).send(weatherArray);
-
-//   } catch (err) {
-//     console.log('ERROR', err);
-//     response.status(500).send('sorry, we messed up');
-//   }
-
-// })
-
-// function Weather(obj) {
-//   this.forecast = obj.weather.description;
-//   this.time = obj.valid_date;
-//   // array.push(this)
-// }
-
-app.get('*', (request, response) => {
-  response.status(404).send('sorry, this route does not exist');
-})
-
-//WEATHER
-// app .get('/weather', response) =>{
-//   let search_query
-// }
-
-function Location(searchQuery, obj) {
+function Location(searchQuery, obj){
   this.search_query = searchQuery;
   this.formatted_query = obj.display_name;
   this.latitude = obj.lat;
   this.longitude = obj.lon;
 }
+//WEATHER
+app.get('/weather', (request, response) => {
+  let search_query = request.query.search_query;
+  // console.log('stuff I got from the front end on the weather route', search_query);
+
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${search_query}&key=${process.env.WEATHER_API_KEY}`;
+
+  superagent.get(url)
+    .then(resultsFromSuperAgent => {
+      let weatherResult = resultsFromSuperAgent.body.data.map(day =>{
+        return new Weather(day);
+
+      })
+      response.status(200).send(weatherResult);
+    }).catch(err => console.log(err));
+})
+
+
+function Weather(obj) {
+  this.forecast = obj.weather.description;
+  this.time = obj.valid_date;
+  // array.push(this)
+}
+
+
+//HIKING
+
+app.get('/trails', (request,response) => {
+  try{
+
+    let latitude = request.query.latitude;
+    let longitude = request.query.longitude;
+
+    let url = `https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=10&key=${process.env.TRAIL_API_KEY}`;
+
+    superagent.get(url)
+      .then(resultsFromSuperAgent => {
+        let hikingResults = resultsFromSuperAgent.body.trails.map(hike => {
+          return new Hiking(hike);
+        })
+        console.log(hikingResults)
+        response.status(200).send(hikingResults);
+      }).catch(err=> console.log(err))
+  } catch(err) {
+    console.log(err);
+    response.status(500).send('Sorry, somethimg went wrong');
+  }
+})
+
+function Hiking(obj) {
+  this.name=obj.name;
+  this.location=obj.location;
+  this.length=obj.length;
+  this.stars=obj.stars;
+  this.star_votes=obj.starVotes;
+  this.summary=obj.summary;
+  this.trail_url=obj.url;
+  this.conditions=`${obj.conditionDetails || ''} ${obj.conditionStatus}`;
+  this.conditions_date=obj.conditionDate.slice(0,obj.conditionDate.indexOf(' '));
+  this.conditions_time=obj.conditionDate.slice(obj.conditionDate.indexOf(' ')+1,obj.conditionDate.length);
+}
+
+
+app.get('*', (request, response) => {
+  response.status(404).send('sorry, this route does not exist');
+})
+
 
 // turn on the lights - move into the house - start the server
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
-
